a24e427e4cc1de99de8c7a234115be722c39869e
shabhias <shabhialiyya3101@gmail.com>
2024-03-23 21:08:20 +0700

    (2) Returning HTML

--- hello/README.md
+++ hello/README.md
@@ -1,4 +1,4 @@
-# REFLECTION
+# REFLECTION MODULE 6
 
 ## COMMIT 1
 
@@ -8,3 +8,16 @@ Handle Connection:
 - Membaca baris-baris yang telah di buffer sampai mencapai baris kosong
 - Mewakili permintaan hrrp dengan mengumpulkan baris-baris tersebut menjadi vektor
 - Mencetak permintaan HTTP untuk inspeksi. handle_connection membaca aliran TCP masuk baris per-baris sampai menemukan baris kosong yang menandakan akhir dari header permintaan HTTP. Lalu, mencetak baris-baris yang terkumpul sebagai permintaan HTTP.
+
+
+## COMMIT 2
+
+Fungsi handle connection yang telah di update:
+- Menghandle status_line yang menunjukkan bahwa respons HTTP adalah 200 OK, yang berarti permintaan telah berhasil diproses.
+- Membaca konten dari file yang bernama hello.html dan mengubahnya menjadi string menggunakan fs::read_to_string(). Ini mengasumsikan bahwa file hello.html berada di direktori yang sama dengan program yang sedang berjalan.
+- Menghitung panjang dari string yang berisi konten file.
+- Menyiapkan format respons HTTP, yang mencakup baris status, panjang konten, dan konten dari file hello.html.
+- Menulis respons tersebut kembali ke aliran TCP dan mengirimkannya ke klien menggunakan write_all().
+
+<a href="https://ibb.co/b7d1PCy"><img src="https://i.ibb.co/qx1Bdc4/Screenshot-2024-03-23-210521.png" alt="Screenshot-2024-03-23-210521" border="0"></a>
+
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,15 +1,38 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() { let stream = stream.unwrap();
-        handle_connection(stream); }
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
 }
 
-fn handle_connection(mut stream: TcpStream) { let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader .lines() .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty()) .collect();
-    println!("Request: {:#?}", http_request);
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<String> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    // Use the http_request here if needed
+    for line in &http_request {
+        println!("{}", line);
+    }
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+    let response = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}",
+        status_line = status_line,
+        length = length,
+        contents = contents
+    );
+    stream.write_all(response.as_bytes()).unwrap();
 }
